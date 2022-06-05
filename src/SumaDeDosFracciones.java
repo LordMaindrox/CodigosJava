85c630cf7526961623dc60c5c64a15b5eb5884a1
maindrox <maindrox446@gmail.com>
2022-06-05 09:37:00 -0500

    actualización

--- src/SumaDeDosFracciones.java
+++ src/SumaDeDosFracciones.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class SumaDeDosFracciones {
+import java.util.Scanner;
+public class SumaDeDosFracciones {
+    public static void main(String[] args) {
+        Scanner scanDato = new Scanner(System.in);
+        System.out.println("Bienvenido al programa de suma de dos fracciones");
+        System.out.println("Escriba el numerador de la primera fracción");
+        int numeradorA = scanDato.nextInt();
+        System.out.println("Escriba el denominador de la primera fracción");
+        int denominadorB = scanDato.nextInt();
+        System.out.println("Escriba el numerador de la segunda fracción");
+        int numeradorC = scanDato.nextInt();
+        System.out.println("Escribe el denominador de la segunda fracción");
+        int denominadorD = scanDato.nextInt();
+        int denominadorSuma = denominadorB*denominadorD;
+        int sumaAD = numeradorA*denominadorD;
+        int sumaBC = numeradorC*denominadorB;
+        int sumaNumerador = sumaAD+sumaBC;
+        System.out.println("El resultado de la suma de las dos fracciones dadas es: "+sumaNumerador+"/"+denominadorSuma);
+
+    }
 }
